0219ee1698947df191638171d07abeb25e3b1e8b
eliret06 <eliret03@gmail.com>
2026-04-17 18:12:56 +0200

    feat: prod ui init

---
+++ Frontend/TeaPotUi/src/util/types.ts
@@ -0,0 +1,59 @@
+export type User = {
+  username: string;
+  displayName?: string;
+  email: string;
+  profileImage?: string;
+  timezone?: string;
+  workCapacityHours?: number;
+  workDays?: string[];
+  workStart?: string; // HH:mm
+  workEnd?: string; // HH:mm
+  breakRules?: string;
+  notifications?: Notifications;
+  orgs: Org[];
+  tasks: Task[];
+  role: Role;
+  invites?: Invitation[];
+};
+
+export type Org = {
+  id: string;
+  name: string;
+  users: User[];
+  adminEmails?: string[];
+  invites?: Invitation[];
+};
+
+export type Calendar = {
+  tasks: Task[];
+};
+
+export type Task = {
+  startDate: Date;
+  endDate: Date;
+  name: string;
+  description: string;
+  isFixed?: boolean;
+  priority?: Priority;
+  status?: "todo" | "in-progress" | "done";
+  org: Org;
+  recurrence?: "none" | "daily" | "weekly";
+  deadline?: Date;
+  dependencies: Task[];
+};
+
+export type Priority = "low" | "medium" | "high";
+
+export type Role = "admin" | "user";
+
+export type Invitation = {
+  orgId: string;
+  orgName: string;
+  email: string;
+  status: "pending" | "accepted" | "declined";
+};
+
+export type Notifications = {
+  emailInvites: boolean;
+  emailDeadlines: boolean;
+};
