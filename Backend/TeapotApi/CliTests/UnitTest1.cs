56776e9aff4a4e8ab342f9cdd0b54a4bf256add0
catschais <david.gr4f@outlook.de>
2026-04-19 17:46:20 +0200

    Cli unit tests

---
+++ Backend/TeapotApi/CliTests/UnitTest1.cs
@@ -0,0 +1,90 @@
+﻿using Cli;
+
+namespace CliTests;
+
+public class CliTests
+{
+    private CommandParser _parser;
+    [SetUp]
+    public void Setup()
+    {
+        _parser = new CommandParser();
+    }
+
+    [Test]
+    public async Task CreateOrganization_Success()
+    {
+        string[] args = { "create-org", "--name", "organization", "--description", "An Organization", "--max-members", "20", "--organizer-username", "John Pork", "--organizer-email", "JohnPork@email.com"};
+        
+        var result = await _parser.ParseCommand(args);
+
+        Assert.That(result, Is.EqualTo(0));
+    }
+
+    [Test]
+    public async Task CreateOrganization_Failure_ZeroArgs()
+    {
+        string[] args = { };
+        Assert.That(async () => await _parser.ParseCommand(args),
+            Throws.TypeOf<Exception>()
+                .With.Message.EqualTo($"No Arguments provided.")
+        );
+    }
+
+    [Test]
+    public async Task CreateOrganization_Failure_UnknownCommand()
+    {
+        string[] args = { "create-team" };
+        Assert.That(async () => await _parser.ParseCommand(args),
+            Throws.TypeOf<Exception>()
+                .With.Message.EqualTo($"Unknown command: {args[0]}"));
+    }
+
+    [Test]
+    public async Task CreateOrganization_Failure_InvalidArguments()
+    {
+        string[] args = { "create-org", "meow" };
+        Assert.That(async () => await _parser.ParseCommand(args),
+            Throws.TypeOf<Exception>()
+                .With.Message.EqualTo("Invalid command line arguments. Check Console for more information.")
+        );
+    }
+
+    [Test]
+    public async Task CreateOrganization_Failure_EmptyArguments()
+    {
+        string[] args = { "create-org", "--name", " ", "--description", "An Organization", "--max-members", "20", "--organizer-username", "John Pork", "--organizer-email", "JohnPork@email.com" };
+        Assert.That(async () => await _parser.ParseCommand(args),
+            Throws.TypeOf<Exception>()
+                .With.Message
+                .EqualTo($"Invalid command line arguments. Make sure you filled out all required parameters.")
+        );
+    }
+
+    [Test]
+    public async Task CreateOrganization_Failure_InvalidMaxMembers()
+    {
+        string[] args =
+        {
+            "create-org", "--name", "organization", "--description", "An Organization", "--max-members", "cat",
+            "--organizer-username", "John Pork", "--organizer-email", "JohnPork@email.com"
+        };
+        Assert.That(async () => await _parser.ParseCommand(args),
+            Throws.TypeOf<Exception>()
+                .With.Message.EqualTo($"Argument for max-members must be a whole positive number.")
+        );
+    }
+
+    [Test]
+    public async Task CreateOrganization_Failure_NegativeMaxMembers()
+    {
+        string[] args = {
+            "create-org", "--name", "organization", "--description", "An Organization", "--max-members", "-20",
+            "--organizer-username", "John Pork", "--organizer-email", "JohnPork@email.com"
+        };
+        Assert.That(async () => await _parser.ParseCommand(args),
+            Throws.TypeOf<Exception>()
+                .With.Message.EqualTo($"Failed to create organization. Check the console for more information.")
+        );
+    }
+}
