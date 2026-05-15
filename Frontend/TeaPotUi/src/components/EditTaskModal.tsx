da40c91cf339c024114aa5286260c24b6839db7f
catschais <david.gr4f@outlook.de>
2026-05-15 10:13:49 +0200

    made modal scrollable more convinient

--- Frontend/TeaPotUi/src/components/EditTaskModal.tsx
+++ Frontend/TeaPotUi/src/components/EditTaskModal.tsx
@@ -102,12 +102,15 @@ const EditTaskModal: FC<EditTaskModalProps> = ({ task, onClose }) => {
       className="fixed inset-0 z-50 flex items-center justify-center bg-black/50"
     >
       <div
-        className="bg-slate-900 rounded-3xl p-6 shadow-xl w-full max-w-lg max-h-[100vh] overflow-y-auto"
+        className="bg-slate-900 rounded-3xl p-6 shadow-xl w-full max-w-lg max-h-[100vh] flex flex-col"
         onClick={(e) => e.stopPropagation()}
       >
-        <h2 className="text-xl font-semibold text-slate-50 mb-4">Edit Task</h2>
+        <div className= "p-1">
+          <h2 className="text-xl font-semibold text-slate-50 mb-4">Edit Task</h2>
         {error && <div className="text-sm text-rose-300 mb-2">{error}</div>}
-        <div className="grid grid-cols-1 gap-3">
+        </div>
+        <div className="p-4 overflow-y-auto flex-1">
+          <div className="grid grid-cols-1 gap-3">
           <label className="text-xs tracking-[0.14em] text-slate-500 uppercase">Title</label>
           <input
             value={form.name}
@@ -213,7 +216,9 @@ const EditTaskModal: FC<EditTaskModalProps> = ({ task, onClose }) => {
             </div>
           </label>
         </div>
-        <div className="mt-4 flex justify-end gap-2">
+        </div>
+        <div className="p-1">
+            <div className="mt-4 flex justify-end gap-2">
           <button
             onClick={handleDelete}
             className="rounded-xl border border-red-300/60 bg-red-500/10 py-1 font-semibold text-red-100 hover:bg-red-500/20 mr-auto"
@@ -232,6 +237,7 @@ const EditTaskModal: FC<EditTaskModalProps> = ({ task, onClose }) => {
           >
             Save
           </button>
+        </div>
         </div>
       </div>
     </div>,
